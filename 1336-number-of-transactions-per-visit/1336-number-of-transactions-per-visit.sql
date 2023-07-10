3b5f376f375cacaabecd16537b2ea1c6bfac7bfd
Manikanta Sanjay Veera <53619729+ManikantaSanjay@users.noreply.github.com>
2023-07-10 11:47:12 +0530

    Time: 1884 ms (54.33%), Space: 0B (100.00%) - LeetHub

---
+++ 1336-number-of-transactions-per-visit/1336-number-of-transactions-per-visit.sql
@@ -0,0 +1,24 @@
+WITH recursive seq AS (
+    SELECT 0 AS transactions_count
+    UNION ALL
+    SELECT transactions_count + 1
+    FROM seq
+    WHERE transactions_count < (SELECT MAX(transactions_count) FROM (
+        SELECT V.user_id, V.visit_date, COUNT(T.transaction_date) AS transactions_count
+        FROM Visits V
+        LEFT JOIN Transactions T
+        ON V.user_id = T.user_id AND V.visit_date = T.transaction_date
+        GROUP BY V.user_id, V.visit_date) AS max_trans)
+),
+trans_count AS (
+    SELECT V.user_id, V.visit_date, COUNT(T.transaction_date) AS transactions_count
+    FROM Visits V
+    LEFT JOIN Transactions T
+    ON V.user_id = T.user_id AND V.visit_date = T.transaction_date
+    GROUP BY V.user_id, V.visit_date
+)
+SELECT seq.transactions_count, COALESCE(COUNT(trans_count.user_id), 0) AS visits_count
+FROM seq
+LEFT JOIN trans_count ON seq.transactions_count = trans_count.transactions_count
+GROUP BY seq.transactions_count
+ORDER BY seq.transactions_count;
