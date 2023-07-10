2998217bca730d21379d80726a47a1edffea0dc0
Manikanta Sanjay Veera <53619729+ManikantaSanjay@users.noreply.github.com>
2023-07-10 12:30:21 +0530

    Time: 1077 ms (6.23%), Space: 0B (100.00%) - LeetHub

---
+++ 1369-get-the-second-most-recent-activity/1369-get-the-second-most-recent-activity.sql
@@ -0,0 +1,11 @@
+SELECT username, activity, startDate, endDate
+FROM (
+  SELECT username, activity, startDate, endDate,
+    ROW_NUMBER() OVER (PARTITION BY username ORDER BY endDate DESC) AS rn
+  FROM UserActivity
+) tmp
+WHERE rn = 2 OR (rn = 1 AND NOT EXISTS (
+    SELECT 1 FROM UserActivity
+    WHERE username = tmp.username AND endDate < tmp.endDate))
+    
+ORDER BY username, rn DESC;
