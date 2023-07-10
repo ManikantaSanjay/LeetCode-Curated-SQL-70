7bfbd197d272cba9810df850520cf4917798bdd8
Manikanta Sanjay Veera <53619729+ManikantaSanjay@users.noreply.github.com>
2023-07-10 13:19:45 +0530

    Time: 1052 ms (82.41%), Space: 0B (100.00%) - LeetHub

--- 1412-find-the-quiet-students-in-all-exams/1412-find-the-quiet-students-in-all-exams.sql
+++ 1412-find-the-quiet-students-in-all-exams/1412-find-the-quiet-students-in-all-exams.sql
@@ -1,39 +1,25 @@
-# Write your MySQL query statement below
-SELECT
-    S.student_id,
-    S.student_name
-FROM
-    Student S
-WHERE
-    S.student_id NOT IN (
-        SELECT
-            E.student_id
-        FROM
-            Exam E
-        WHERE
-            E.score IN (
-                SELECT
-                    MAX(score)
-                FROM
-                    Exam
-                WHERE
-                    exam_id = E.exam_id
-                
-                UNION
-                
-                SELECT
-                    MIN(score)
-                FROM
-                    Exam
-                WHERE
-                    exam_id = E.exam_id
-            )
-    )
-    AND S.student_id IN (
-        SELECT DISTINCT
-            student_id
-        FROM
-            Exam
-    )
-ORDER BY
-    S.student_id;
+-- Create a common table expression (CTE) 'main' that includes each student's ranks in each exam based on their scores
+WITH main AS (
+    SELECT 
+        student_id,
+        -- Rank students within each exam based on their scores in ascending order
+        RANK() OVER (PARTITION BY exam_id ORDER BY score ASC) AS rank_asc,
+        -- Rank students within each exam based on their scores in descending order
+        RANK() OVER (PARTITION BY exam_id ORDER BY score DESC) AS rank_desc
+    FROM 
+        exam
+) 
+
+-- From the 'student' table, select students who never had the best or worst score in any exam
+SELECT * 
+FROM student
+WHERE student_id IN (
+    -- In the subquery, we are grouping by student_id and making sure that the minimum rank in either ascending or descending order is greater than 1
+    -- This ensures that the student did not get the highest or lowest score in any of the exams
+    SELECT student_id 
+    FROM main 
+    GROUP BY student_id 
+    HAVING MIN(rank_asc) > 1 AND MIN(rank_desc) > 1
+)
+ORDER BY 
+    student_id;
